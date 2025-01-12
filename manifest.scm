654dc7b396437bc45f11d2085707cc00604fb132
gemmaro <gemmaro.dev@gmail.com>
2025-01-12 09:17:05 +0900

    Install LibXML2 in Guix manifest.

--- manifest.scm
+++ manifest.scm
@@ -1,1 +1,4 @@
-(specifications->manifest (list "libxslt" "ruby@3.1" "ruby-nokogiri"))
+(specifications->manifest (list "libxslt" ;xsltproc
+                                "libxml2" ;xmllint
+                                "ruby@3.1"
+                                "ruby-nokogiri"))
